37bca3badfb4b185f8786ae3eb957761e89fd66f
Robert Churches <rchurches@immanuel.sa.edu.au>
2019-04-03 16:54:58 +1030

    3rdapril1645

---
+++ array/array/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.btnDetails = New System.Windows.Forms.Button()
+        Me.btnQuery = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(497, 90)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(159, 69)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Name Input"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(53, 74)
+        Me.txtUser.Multiline = True
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtUser.Size = New System.Drawing.Size(193, 301)
+        Me.txtUser.TabIndex = 1
+        '
+        'btnDetails
+        '
+        Me.btnDetails.Location = New System.Drawing.Point(497, 165)
+        Me.btnDetails.Name = "btnDetails"
+        Me.btnDetails.Size = New System.Drawing.Size(159, 51)
+        Me.btnDetails.TabIndex = 2
+        Me.btnDetails.Text = "Show user details"
+        Me.btnDetails.UseVisualStyleBackColor = True
+        '
+        'btnQuery
+        '
+        Me.btnQuery.Location = New System.Drawing.Point(497, 234)
+        Me.btnQuery.Name = "btnQuery"
+        Me.btnQuery.Size = New System.Drawing.Size(159, 51)
+        Me.btnQuery.TabIndex = 3
+        Me.btnQuery.Text = "Query"
+        Me.btnQuery.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnQuery)
+        Me.Controls.Add(Me.btnDetails)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents btnDetails As Button
+    Friend WithEvents btnQuery As Button
+End Class
---
+++ array/array/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Public username(5), nickname(5) As String
+    'declares an array of 5 for variables username and their Nick names
+    'These are global to the form
+    'An array of five has indexes of 0 to 4 NOT 1 to 5
+    Private Sub btnDetails_Click(sender As Object, e As EventArgs) Handles btnDetails.Click
+        For count = 0 To 4
+            txtUser.Text += username(count) & "'s Nick name is " & nickname(count) & vbCrLf
+        Next
+    End Sub
+
+    Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
+        'Dim userquery As String
+        'Dim indexnum, indx As Integer
+        'userquery = InputBox("Enter the user's name to find their Nickname")
+        'indx = username.IndexOf(
+        'Dim arr As String() = {"aa", "bb", "cc"}
+        'Dim indx As Integer
+        ' indx = array.IndexOf(arr, "bb")
+        ' array
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        For i = 0 To 4
+            username(i) = InputBox("What is your Name?", "UserName")
+            nickname(i) = InputBox(username(i) & ", What is your Nickname?", "NickName")
+        Next
+
+    End Sub
+End Class
